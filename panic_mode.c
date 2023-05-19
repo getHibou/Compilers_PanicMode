ab1f9da670f5c3e8ddb28a1b1b915d276c1fb35b
João Pedro <eendevJP@proton.me>
2023-05-19 11:31:25 -0300

    Update panic_mode.c

--- panic_mode.c
+++ panic_mode.c
@@ -5,44 +5,44 @@
 
 char token;
 
-int somas(void); // Soma e SubtraÁ„o 
-int produto(void); // MultiplicaÁ„o 
-int num(void); // N˙meros ou Sub-expressıes
+int somas(void); // Soma e Subtra√ß√£o 
+int produto(void); // Multiplica√ß√£o 
+int num(void); // N√∫meros ou Sub-express√µes
 
-void error_Parenteses(void){ //Quando h· mais de um parÍnteses ou falta um na express„o
-	fprintf(stderr,"\nEst· faltando um parÍntesis na express„o\n");
+void error_Parenteses(void){ //Quando h√° mais de um par√™nteses ou falta um na express√£o
+	fprintf(stderr,"\nEst√° faltando um par√™ntesis na express√£o\n");
 }
-void error_Divisao(void){ //Quando colocada, n„o permitida
-	fprintf(stderr,"\nErro: Operador para divis„o n„o È permitido\n");
+void error_Divisao(void){ //Quando colocada, n√£o permitida
+	fprintf(stderr,"\nErro: Operador para divis√£o n√£o √© permitido\n");
 }
-void error_Porcentagem(void){ //Quando colocada, n„o permitida
-	fprintf(stderr,"\nErro: Operador para resto de divis„o n„o È permitido\n");
+void error_Porcentagem(void){ //Quando colocada, n√£o permitida
+	fprintf(stderr,"\nErro: Operador para resto de divis√£o n√£o √© permitido\n");
 }
-void error_EntradaChar(void){ //Quando colocada, n„o permitida
-  fprintf(stderr,"\nErro: Entrada com n˙mero e caractere n„o È permitido\n");
+void error_EntradaChar(void){ //Quando colocada, n√£o permitida
+  fprintf(stderr,"\nErro: Entrada com n√∫mero e caractere n√£o √© permitido\n");
 }
-void error_EntradaEspecial(void){ //Quando colocado um caracter especial, n„o È permitido
-  fprintf(stderr,"\nErro: Entrada com caracter especial n„o È permitido\n");
+void error_EntradaEspecial(void){ //Quando colocado um caracter especial, n√£o √© permitido
+  fprintf(stderr,"\nErro: Entrada com caracter especial n√£o √© permitido\n");
 }
 
 void match(char tokenEsperado){
-	if(token == tokenEsperado){
-    token = getchar();
+   if(token == tokenEsperado){
+   token = getchar();
   }else{
-    error_Parenteses();
-    while(!isdigit(token))  
-    token = getchar();
+   error_Parenteses();
+   while(!isdigit(token))  
+   token = getchar();
   }
 }
 
 int main(void) {
   setlocale(LC_ALL,"Portuguese");
-	int result;
-  printf("\nDigite aqui a express„o: \n");
+  int result;
+  printf("\nDigite aqui a express√£o: \n");
   token = getchar();
   result = somas();
   if(token == '\n'){
-  	printf("\nResultado = %d\n", result);
+     printf("\nResultado = %d\n", result);
   }else{
     if(isalpha(token)){
       error_EntradaChar();
@@ -54,49 +54,45 @@ int main(void) {
     if(token == '%'){
       error_Porcentagem();
     }
-    //if(!isanum(token)){
-      //error_EntradaEspecial();
-    //}
   }
 }
 
 int produto(void){
 	int temp = num();
 	while(token == '*'){
-		match('*');
-		temp *= num();
+	match('*');
+	temp *= num();
 	}
 	return temp;
 }
 
 int somas(void){
   int temp = produto();
-	while((token == '+') || (token == '-')){
-		switch(token){
-			case '+': 
+  while((token == '+') || (token == '-')){
+      switch(token){
+	case '+': 
           match('+');
-					temp += produto();
-					break;
-			case '-': 
+	  temp += produto();
+	  break;
+	case '-': 
           match('-');
-					temp -= produto();
-					break;
-		}
+	  temp -= produto();
+	  break;
 	}
+}
   return temp;
 }
 
 int num(void){
 	int temp;
-
 	if (token == '('){
-		match ('(');
-		temp = somas();
-		match(')');
+	    match ('(');
+	    temp = somas();
+	    match(')');
 	}else if (isdigit(token)){
-		ungetc(token,stdin);
-		scanf("%d", &temp);
-		token = getchar();
+	   ungetc(token,stdin);
+	   scanf("%d", &temp);
+	   token = getchar();
 	}
   return temp;
 }
